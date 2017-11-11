d3f14cec030fe25c7893b37e430faa4989349965
amongleaves <vaidotas.valiukas7@yandex.com>
2017-11-11 22:05:21 +0200

    fix app.js

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.get('/weathernow/', function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Server has been started.")
 });
